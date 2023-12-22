2cd1abdf1a6781c39a824aa02af3b8120dd40309
Julian John <the@ge.de>
2023-12-22 13:29:24 +0100

    added the general calculator, with the Ui_interface

---
+++ calculator/General_Calculator.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+class UiInterface:
+    """
+    The UiInterface is an Interface to decouple the calculation from the UI.
+    With an Object of the UiInterface, you can define what should be done with the probabilities you hand over.
+    The interface is recursively defined to support infinite complex combinations.
+    """
+    def return_value(self):
+        pass
+
+
+class ProbPair(UiInterface):
+    """
+    The Prob(ability)Pair is the atomic unit. It is the pair of the absolute frequency of an event and the reference
+    associated with it.
+    """
+    def __init__(self, abs_frequency, reference):
+        self.abs_frequency = abs_frequency
+        self.reference = reference
+
+    def return_value(self):
+        return self
+
+    def return_relative_prob(self):
+        return self.abs_frequency / self.reference
+
+
+class PlusCombination(UiInterface):
+    """
+    A PlusCombination is needed when you want to combine features that are from the same feature category.
+    These features are handed over in a List.
+    For example: You want the probability of an accident from 18-25 and 25-65. These two Feature are from the category
+    age. Hence, they are combined with PlusCombination.
+    """
+    def __init__(self, freq_list: [UiInterface]):
+        self.freq_list = freq_list
+
+    def return_value(self):
+        resolved_list = [pair.return_value() for pair in self.freq_list]
+        new_abs_freq = 0
+        new_reference = 0
+        for pair in resolved_list:
+            new_abs_freq += pair.abs_frequency
+            new_reference += pair.reference
+
+        return ProbPair(new_abs_freq, new_reference)
+
+
+class AndCombination(UiInterface):
+    """
+    An AndCombination is needed if you want to combine features that are from different feature categories.
+    They are handed over in a list.
+    For example: You want the probability of an accident between 18-25 and when you are male. These two features qre not
+    from the same category and hence, combined with AndCombination.
+    """
+    def __init__(self, freq_list: [UiInterface]):
+        self.freq_list = freq_list
+
+    def return_value(self):
+        resolved_list = [pair.return_value() for pair in self.freq_list]
+        new_abs_freq = 1
+        new_reference = 1
+        for pair in resolved_list:
+            new_abs_freq *= pair.abs_frequency
+            new_reference *= pair.reference
+
+        return ProbPair(new_abs_freq, new_reference)
+
+
+def calculate_first_accident_prob(prob_combination: UiInterface):
+    prob_pair: ProbPair = prob_combination.return_value()
+    yearly_prob = prob_pair.return_relative_prob()
+
+    prob_accident = np.array(
+        [np.prod(1 - yearly_prob[:year_index]) * yearly_prob[year_index] for year_index in range(len(yearly_prob))]
+    )
+
+    return prob_accident
