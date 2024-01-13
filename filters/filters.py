09201715cc8ebb97ef71b3a49c6b2d1da9d908e1
yannick <yannj96@googlemail.com>
2024-01-13 13:36:15 +0100

    Reduced filter set with allow instead of deny list

--- filters/filters.py
+++ filters/filters.py
@@ -18,7 +18,7 @@ class Filters:
 
     # getNextQuestion(): {question_key, message_text, message_options[]}
     def get_next_question(self):
-        if(not self.hasNextQuestion()):
+        if(not self.has_next_question()):
             return {}
         for key, value in self.filter_data.items():
             answer_options = []
@@ -30,20 +30,33 @@ class Filters:
     # setQuestionAnswer(message_key, option_key): void
     def set_question_answer(self, message_key, option_key):
         self.selected_options.append((message_key,option_key))
-        # remove options based on answer
-        if(option_key != "skip"):
-            current_question = self.filter_data[message_key]
-            answer_options = current_question["answer_options"]
-            answer_option = answer_options[option_key]
-            for restricts_question in answer_option["restricts_questions"]:
-                if restricts_question in self.filter_data:
-                    del self.filter_data[restricts_question]
-            # TODO delete single options
+        current_question = self.filter_data[message_key]
         del self.filter_data[message_key]
+        if(option_key == "skip"):
+            return
+        
+
+        # remove options based on answer
+        answer_options = current_question["answer_options"]
+        answer_option = answer_options[option_key]
+        allowed_questions = answer_option["allowed_questions"]
+        for question in dict(self.filter_data):#iterate on copy to allow modifications
+            if question in allowed_questions[:]:
+                question_allowed_options = answer_option["allowed_options"]
+                if question not in question_allowed_options:
+                    continue
+                allowed_options = question_allowed_options[question]
+                for option_key, option_details in question[answer_options].items():
+                    if option_key not in allowed_options:
+                        del question[answer_options][option_key]
+            else:
+                del self.filter_data[question]
 
     def get_all_filters(self):
         return (self.selected_options,self.filter_data)
-        
+    
+    def __str__(self):
+        return "\"filter\":{\"selected_options\"="+str(self.selected_options)+",\"filter_data="+str(self.filter_data)+",\"initial_filter_data\"="+str(self.initial_filter_data)+"}"
             
 
 class Question:
@@ -51,8 +64,17 @@ class Question:
         self.question_key=question_key
         self.question_text=question_text
         self.question_options=question_options
-
+    def __str__(self):
+        return "\"question\":{\"key\"="+self.question_key+", \"text\"= "+self.question_text+", \"options\"="+str(self.question_options)+"}"
+   
 class AnswerOption:
     def __init__(self,option_key,option_text):
         self.option_key=option_key
         self.option_text=option_text
+    def __str__(self):
+        return "AnswerOption(\nkey = "+self.option_key+", \ntext= "+self.option_text+")"
+
+test = Filters()
+print(test.has_next_question())
+test.set_question_answer("damage","lightly_injured")
+print(test)
