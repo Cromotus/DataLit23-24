b320bffac7f366c5a19c8b8dbe10690c31015e84
Julian John <the@ge.de>
2024-01-15 18:14:16 +0100

    added quick plot function for a simple central plot

--- calculator/simple_utils.py
+++ calculator/simple_utils.py
@@ -1,6 +1,7 @@
 from filters import Filters
 from DataLoader import DataContainer
 from General_Calculator import ProbPair
+from matplotlib import pyplot as plt
 
 
 def start_text_ui():
@@ -15,6 +16,12 @@ def start_text_ui():
     csv_name, column_name = filters_container.get_resulting_dataset()
     return data_container.data[csv_name]["Year"], ProbPair(data_container.data[csv_name][column_name],
                                                            data_container.data["Reference Numbers.csv"]["total registered vehicles"])
+
+
+def quick_plot(years, data):
+    fig, ax = plt.subplots()
+    ax.plot(years, data)
+    plt.show()
 
 
 if __name__ == '__main__':
